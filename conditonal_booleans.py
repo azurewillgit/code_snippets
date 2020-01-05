5546ca8f0cdc718689348ef9cac52b624fcc1e6f
azurewillgit <59409752+azurewillgit@users.noreply.github.com>
2020-01-05 09:42:52 +0100

    Add files via upload

---
+++ conditonal_booleans.py
@@ -0,0 +1,11 @@
+
+some_condition = True
+if some_condition == True:
+    print('Success!')
+
+if some_condition:
+    print('Success!')
+
+credit_card_overdraft = True
+if credit_card_overdraft:
+    print("Sorry, you are in debt already!")
